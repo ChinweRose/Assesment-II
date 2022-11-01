6c7518e3934ddb03cc0098a9316e79fb8cb8cec3
Chinwe <103608228+ChinweRose@users.noreply.github.com>
2022-11-01 11:49:52 +0100

    I printed out java

---
+++ src/Task1.java
@@ -0,0 +1,31 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+
+        System.out.println("    j");
+        System.out.println("    j");
+        System.out.println("j   j");
+        System.out.println("  jj");
+        System.out.println();
+
+        System.out.println("       a ");
+        System.out.println("    a     a ");
+        System.out.println("  a a a a a a");
+        System.out.println("a             a");
+        System.out.println();
+        System.out.println();
+
+        System.out.println("v           v");
+        System.out.println("  v       v");
+        System.out.println("    v   v");
+        System.out.println("      v    ");
+        System.out.println();
+
+        System.out.println("       a ");
+        System.out.println("    a     a ");
+        System.out.println("  a a a a a a");
+        System.out.println("a             a");
+        System.out.println();
+
+    }
+}
